0c8001f447ef06b795e9dbfefe68edf8b0442eb6
id <ismail.demir@emikon.com.tr>
2023-11-20 09:22:52 +0300

    Added Scaffold

---
+++ EFCore.DatabaseFirst.Scaffold/Models/DbEfcoreDbFirstContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.DatabaseFirst.Scaffold.Models;
+
+public partial class DbEfcoreDbFirstContext : DbContext
+{
+    public DbEfcoreDbFirstContext()
+    {
+    }
+
+    public DbEfcoreDbFirstContext(DbContextOptions<DbEfcoreDbFirstContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=FE-NB001;Initial Catalog=DB_EFCoreDbFirst;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Name)
+                .HasMaxLength(10)
+                .IsFixedLength();
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EFCore.DatabaseFirst.Scaffold/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using EFCore.DatabaseFirst.Scaffold.Models;
+using Microsoft.EntityFrameworkCore;
+
+using (var _context = new DbEfcoreDbFirstContext())
+{
+    var products = await _context.Products.ToListAsync();
+
+
+    foreach (var product in products)
+    {
+        Console.WriteLine(product.Id + ":" + product.Name + " : " + product.Price);
+    }
+
+}
+
